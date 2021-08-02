881550a76a7322a8b6d005d6e87a445d8fcc2c66
yangbq <yangbq@qq.com>
2021-08-02 17:30:59 +0800

    Money页面布局

--- src/App.vue
+++ src/App.vue
@@ -3,6 +3,13 @@
     <router-view/>
   </div>
 </template>
+<style lang="scss">
+*{
+  margin: 0;padding: 0;
+  box-sizing: border-box;
+}
+
+</style>
 
 <style lang="scss">
 #app {
--- src/views/Money.vue
+++ src/views/Money.vue
@@ -1,6 +1,8 @@
 <template>
-    <div>
+    <div class="nav-wrapper">
+        <div class="content">
         Money.vue
+        </div>
         <Nav/>
     </div>
 </template>
@@ -11,5 +13,15 @@ export default {
 };
 </script>
 <style lang="scss" scoped>
+.nav-wrapper{
+    border: 1px solid green;
+    display: flex;
+    flex-direction: column;
+    height: 100vh;
+}
+.content{
+    overflow: auto;
+    flex-grow: 1;
+}
 
 </style>
